9b742432f40723a6e67070912f870adde0f6bd41
Nicola Anesini <nicola.anesini@hotmail.com>
2019-12-09 08:30:50 +0100

    V 0.2.1 erste Version die läuft

--- Kleiner-Helfer/src/Kleiner-Helfer.c
+++ Kleiner-Helfer/src/Kleiner-Helfer.c
@@ -1,13 +1,14 @@
 /*
  ============================================================================
  Name        : Kleiner-Helfer.c
- Author      : Nicola Anesini,Seam Reinmann
+ Author      : Nicola Anesini,Mauris Otto
  Version     : v0,1
  Copyright   : Your copyright notice
  Description : Kleienr Helfer für Analaysi
 
  Version 	Datum 		Initalen		Was
- 0.1		22:37		NA				Hauptmenu
+ 0.1		03.12		NA				Hauptmenu
+ 0.2		04.12		NA				Modul Menge läuft
  ============================================================================
  */
 // Benutze Libarys
@@ -15,58 +16,69 @@
 #include <stdlib.h>
 
 // Benutze Header
+#include "analysi.h"
 
 
 // Definiert Variabeln
 int Auswahl;
 int Stop_Scheilfe;
+char i;
 
 int main(void) {
 
 	printf("Willkomen in der Welt von Analaysis\n");
 	printf("Das kann ich dir bieten\n");
-	printf("\t -1- \n");
-	printf("\t -2- \n");
-	printf("\t -3- \n");
-	printf("\t -4- \n");
-	printf("\t -5- \n");
-	printf("\t -6- \n");
-	printf("\t -7- \n");
-	printf("\t -0- \n");
+
+	while (i != 'e'){
+	printf("\t -1- Menge \n");
+	printf("\t -2- Funktion\n");
+	printf("\t -3- Reihen,Folgen & Grenzwerte\n");
+	printf("\t -4- Elemtarfunktionen\n");
+	printf("\t -5- Differentzialrechnung\n");
+	printf("\t -6- Integralrechnung\n");
+	printf("\t -7- Kurvendiskusion\n");
+	printf("\t -8- Mulitable-Choosie Fragen\n");
+	printf("\t -0- Zurueck zum Hauptmenu\n");
 	printf("Was draf es sein ?");
 	scanf("%d",&Auswahl);
 
 	switch (Auswahl){
 	 case 1:
-	   printf("1");
-	   break;
+		 printf("1");
+		 Menge();
+	   	 break;
 	 case 2:
-	   printf("2");
-	   break;
+		 printf("2");
+	   	 break;
 	 case 3:
-	   printf("3");
-	   break;
+		 printf("3");
+	   	 break;
 	 case 4:
-	   printf("4");
-	   break;
+		 printf("4");
+		 break;
 	 case 5:
-	   printf("5");
-	   break;
+		 printf("5");
+		 break;
 	 case 6:
-	   printf("6");
-	   break;
+		 printf("6");
+	   	 break;
 	 case 7:
-	   printf("7");
-	   break;
+		 printf("7");
+		 break;
+	 case 8:
+		 printf("8");
+		 break;
 	 case 0:
 	   printf("0");
+	   i = 'e';
+	   prinft("Danke das sie unser System genutz haben");
 	   break;
 	 default:
 
 	   break;
 	}
 
-
+	}
 
 
 
